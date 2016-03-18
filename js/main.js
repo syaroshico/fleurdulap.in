3a1741b9c6db6e983b3dd03e540a4c38dfb2777d
6h1sa <inekomugi@gmail.com>
2016-03-18 12:48:28 +0900

    Renew
    Update Sight disign

--- js/main.js
+++ js/main.js
@@ -1,91 +1,94 @@
-$(function() {
-  var mainContent = $('.mainContent');
-  var navMenuBtn = $('h2.menu');
-  var menu = $('.slideMenu');
-  var slideMenuBtn = $('.slideMenuBtn');
-  var slideMenuLayer = $('.slideMenuLayer');
-  var body = $('.box,.box3');
+function getBrowser() {
+  var ua = window.navigator.userAgent.toLowerCase();
+  var ver = window.navigator.appVersion.toLowerCase();
+  var name = 'unknown';
+  if (ua.indexOf('msie') != -1) {
+    if (ver.indexOf('msie 6.') != -1) {
+      name = 'ie6';
+    }else if (ver.indexOf('msie 7.') != -1) {
+      name = 'ie7';
+    }else if (ver.indexOf('msie 8.') != -1) {
+      name = 'ie8';
+    }else if (ver.indexOf('msie 9.') != -1) {
+      name = 'ie9';
+    }else if (ver.indexOf('msie 10.' != -1)) {
+      name = 'ie10';
+    }else {
+      name = 'ie';
+    }
+  }
+  return name;
+}
+window.addEventListener('DOMContentLoaded', function() {
+  var articleEle = document.getElementsByClassName('article');
+  var navLinkEle = document.getElementsByClassName('nav__link');
 
-  function moveMenu() {
-    var menuWidth = menu.outerWidth();
-    if (menu.hasClass('menu-open')) {
-      // 閉じる
-      menu.animate({
-        'left': -menuWidth
-      }, 300);
-      slideMenuBtn.animate({
-        'left': 0
-      }, 300);
-      // スマホタッチ有効
-      slideMenuLayer.off('.noScroll');
-      slideMenuLayer.hide();
-      // animateを待ってからelement.styleを削除. 画面サイズ変更対策.
-      setTimeout(function() {
-        menu.css('left', '');
-        slideMenuBtn.css('left', '');
-      }, 302);
-      menu.removeClass('menu-open');
-      body.removeClass('menu-open');
-    } else {
-      // 開く
-      menu.addClass('menu-open');
-      body.addClass('menu-open');
-      slideMenuLayer.show();
-      menu.animate({
-        'left': 0
-      }, 300);
-      slideMenuBtn.animate({
-        'left': menuWidth
-      }, 300);
-      // スマホタッチ無効
-      slideMenuLayer.on('touchmove.noScroll', function(e) {
-        e.preventDefault();
-      });
-    };
-  };
 
-  // ページ切り替え
-  mainContent.first().fadeIn();
-  navMenuBtn.first().addClass('active');
+  for (var i = 0; i < articleEle.length; i++) {
+    articleEle.item(i).classList.add('article--hide');
+    navLinkEle.item(i).addEventListener('click', (function (j) {
+      return function () {
 
-  navMenuBtn.on('click', function() {
-    var menuNum = navMenuBtn.index(this);
-    mainContent.not(this).css({
-      'position': 'absolute',
-      'display': 'none'
-    });
-    navMenuBtn.removeClass('active');
-    mainContent.eq(menuNum).css({
-      'position': 'static',
-      'display': 'block'
-    });
-    $(this).addClass('active');
+        var showArticle = articleEle.item(j);
+        showArticle.classList.add('article--show');
+        showArticle.classList.remove('article--hide');
+        navLinkEle.item(j).classList.add('nav__link--active');
 
-    // インデックスボタンがクリックされたらメニューを閉じる
-    if (menu.hasClass('menu-open')) {
-      var menuWidth = menu.outerWidth();
-      menu.animate({
-        'left': -menuWidth
-      }, 300);
-      slideMenuLayer.off('.noScroll');
-      slideMenuBtn.animate({
-        'left': 0
-      }, 300);
-      slideMenuLayer.hide();
-      setTimeout(function() {
-        menu.css('left', '');
-      }, 301);
-      menu.removeClass('menu-open');
-      body.removeClass('menu-open');
-    };
-  });
+        for (var k = 0; k < articleEle.length; k++) {
+          if (! articleEle.item(k).isEqualNode(showArticle)) {
+            articleEle.item(k).classList.remove('article--show');
+            navLinkEle.item(k).classList.remove('nav__link--active');
+            articleEle.item(k).classList.add('article--hide');
+          }
+        }
 
-  // メニュー開閉イベント
-  slideMenuBtn.on('click', function() {
-    moveMenu();
-  });
-  slideMenuLayer.on('click', function() {
-    moveMenu();
-  });
+      }
+    })(i), false);
 
+  }
+  articleEle.item(0).classList.remove('article--hide');
+
+  var navEle = document.getElementById('nav');
+  var headerEle = document.getElementById('header');
+  var spacerEle = document.createElement('div');
+  headerEle.appendChild(spacerEle);
+  spacerEle.id = 'spacer';
+  spacerEle.display = 'none';
+
+  window.addEventListener('scroll', function () {
+    var headerH = headerEle.offsetHeight;
+    var navH = navEle.offsetHeight;
+    var scrollValue = document.documentElement.scrollTop || document.body.scrollTop;
+    var i = headerH - navH;
+
+    if ( scrollValue > i ) {
+      navEle.classList.add('nav--fixed');
+      spacerEle.style.display = 'block';
+      spacerEle.style.height = navH + 'px';
+    } else if ( scrollValue < i ) {
+      navEle.classList.remove('nav--fixed');
+      spacerEle.style.display = 'none';
+    }
+  }, false);
+
+  var footerEle = document.getElementById('footer');
+  var mainEle = document.getElementById('main');
+  function stickyFooter() {
+    var headerH = headerEle.offsetHeight;
+    var footerH = footerEle.offsetHeight;
+    var i = headerH + footerH;
+    mainEle.style.minHeight = 'calc(100vh - ' + i + 'px)';
+  }
+  stickyFooter();
+  window.addEventListener('resize', stickyFooter, false);
+
+  if (getBrowser() != 'unknown') {
+    var ele = document.createElement('link');
+    document.getElementsByTagName('head').item(0).appendChild(ele);
+    ele.setAttribute('rel', 'stylesheet');
+    ele.setAttribute('href','css/ie.css');
+    var ele = document.createElement('script');
+    document.getElementsByTagName('body').item(0).appendChild(ele);
+    ele.setAttribute('src', 'js/ie.js');
+  }
 });
